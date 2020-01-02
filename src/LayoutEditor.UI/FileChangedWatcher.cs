6fb978da4fbf2d60a7d21bf1c7a423f0f04653da
Darth Affe <darthaffe@wyrez.org>
2020-01-02 15:06:29 +0100

    Added File watcher to be able to change Led-Images on the fly

---
+++ src/LayoutEditor.UI/FileChangedWatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace LayoutEditor.UI
+{
+    public static class FileChangedWatcher
+    {
+        private static FileSystemWatcher _watcher;
+
+        public static event EventHandler<string> FileChanged;
+
+        public static void SetWatchDirectory(string directory)
+        {
+            _watcher?.Dispose();
+
+            if (Directory.Exists(directory))
+            {
+                _watcher = new FileSystemWatcher(directory);
+                _watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.FileName | NotifyFilters.Size;
+                _watcher.Changed += WatcherHit;
+                _watcher.Created += WatcherHit;
+                _watcher.Deleted += WatcherHit;
+                _watcher.Renamed += WatcherHit;
+                _watcher.EnableRaisingEvents = true;
+            }
+            else
+            {
+                _watcher = null;
+            }
+        }
+
+        private static void WatcherHit(object sender, FileSystemEventArgs e)
+        {
+            FileChanged?.Invoke(null, e.Name);
+        }
+    }
+}
